e163cb976c476cc51488df6fd2bbf1c32be2cc25
rajola-ceo <v3nocyber@gmail.com>
2026-03-24 13:35:30 -0700

    Create index.js

---
+++ js/services/index.js
@@ -0,0 +1,6 @@
+// js/services/index.js
+export { authService } from './authService.js';
+export { userService } from './userService.js';
+export { coinService } from './coinService.js';
+export { leagueService } from './leagueService.js';
+export { teamService } from './teamService.js';
