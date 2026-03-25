e5f8e013ea91d543f93fd5ef1a664d0b6110d5cd
rajola-ceo <v3nocyber@gmail.com>
2026-03-25 08:31:12 -0700

    Delete js/pages directory

--- js/pages/index.js
+++
@@ -1,4 +0,0 @@
-// js/pages/index.js
-export { ProfileSettingsPage } from './profileSettings.js';
-export { LeagueCreatePage } from './leagueCreate.js';
-export { LeagueViewPage } from './leagueView.js';
